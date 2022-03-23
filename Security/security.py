d9acc0b4e2528a650afae597ae50be6522d42a36
Clifton McCook <cliftonmccook@gmail.com>
2022-03-22 22:57:52 -0500

    edited security.py

--- Security/security.py
+++ Security/security.py
@@ -12,19 +12,14 @@ class SecurityManager:
 
     def __init__(self):
         self.salt = b'7139'
+        # self.salt = os.urandom(16)
         self.backend = default_backend()
         self.encryption_key = None
 
     def encrypt(self, message: bytes, key: bytes) -> bytes:
-        """
-        performs symmetric encryption of message using key
-        """
         return Fernet(key).encrypt(message)
 
     def decrypt(self, message: bytes, token: bytes) -> bytes:
-        """
-        performs decryption of symmetrically encrypted message using key
-        """
         return Fernet(token).decrypt(message)
 
     def hashpassword(self, pwd):
@@ -47,3 +42,20 @@ class SecurityManager:
         )
         return kdf
 
+    def encrypt_fields(self, obj):
+        enc_obj = obj
+        enc_obj.name = self.encrypt(enc_obj.name, self.encryption_key)
+        enc_obj.url = self.encrypt(enc_obj.url, self.encryption_key)
+        enc_obj.username = self.encrypt(enc_obj.username, self.encryption_key)
+        enc_obj.password = self.encrypt(enc_obj.password, self.encryption_key)
+        enc_obj.notes = self.encrypt(enc_obj.notes, self.encryption_key)
+        return enc_obj
+
+    def decrypt_fields(self, obj):
+        dec_obj = obj
+        dec_obj.name = self.decrypt(dec_obj.name, self.encryption_key)
+        dec_obj.url = self.decrypt(dec_obj.url, self.encryption_key)
+        dec_obj.username = self.decrypt(dec_obj.username, self.encryption_key)
+        dec_obj.password = self.decrypt(dec_obj.password, self.encryption_key)
+        dec_obj.notes = self.decrypt(dec_obj.notes, self.encryption_key)
+        return dec_obj
